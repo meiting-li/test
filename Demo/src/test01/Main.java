1b158065250facb57d0439d4746a56c3a9b152db
meiting-li <137678849+meiting-li@users.noreply.github.com>
2024-10-14 18:17:38 +0800

    Add files via upload

---
+++ Demo/src/test01/Main.java
@@ -0,0 +1,18 @@
+package test01;
+
+import test01.Alphabetizer;
+import test01.Input;
+
+public class Main {
+    public static void main(String[] args) {
+        Input input = new Input();
+        input.input("D:\\input.txt");
+        Shift shift = new Shift(input.getLineTxt());
+        shift.shift();
+        Alphabetizer alphabetizer = new Alphabetizer(shift.getKwicList());
+        alphabetizer.sort();
+        Output output = new Output(alphabetizer.getKwicList());
+        output.output("D:\\output.txt");
+
+    }
+}
